df6a5077e87594863826caba6593deb1290e2db6
Hemant <hemant.shelar@gmail.com>
2025-05-16 16:27:25 +1000

    Default project.

---
+++ Controllers/TitanicController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace mvc_expressions.Controllers
+{
+    public class TitanicController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ViewBagData = "CustomViewBagDataHotReleoadTest";
+            ViewData["ViewDataData"] = "CustomViewDataDataHotReloadTest";
+            Response.Cookies.Append("CookieItem", "test cookie item");
+            var rvalue = Request.Cookies.Where((kvp) =>
+            {
+                bool bReturnValue = false; ;
+                var svalue = kvp.Key;
+                if (svalue.Equals("CookieItem"))
+                {
+                    bReturnValue = true;
+                }
+                else
+                {
+                    bReturnValue |= false;
+                }
+                return bReturnValue;
+            });
+            var fod = rvalue.FirstOrDefault();
+            return View("Titanic");
+        }
+    }
+}
---
+++ Models/SqlConnectOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace mvc_expressions.Models;
+
+public class SqlConnectOptions
+{
+    public int Id { get; set; }
+    public string ConnectionString { get; set; }
+}
